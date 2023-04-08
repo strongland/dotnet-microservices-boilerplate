b5e142528bba06a2e5458ce8b328d8710cab4917
Jakob Nyström <jakob.nystrom@addiva.se>
2023-04-08 23:04:31 +0200

    cdciservice stubs

---
+++ src/Infrastructure/CDCI/CDCIService.cs
@@ -0,0 +1,35 @@
+﻿using FSH.Core.Common;
+using FSH.Core.Dto.BankId;
+using FSH.Core.Dto.CDCI;
+using FSH.WebApi.Infrastructure.Common.Settings;
+using FSH.WebApi.Infrastructure.Helpers;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Serilog;
+
+namespace FSH.Infrastructure.Auth;
+
+public class CDCIService : ICDCIService {
+
+    private readonly ILogger Logger;
+
+    public CDCIService(IConfiguration config, ILogger logger, IUserService userService) {
+        Logger = logger;
+        Runtime.SecuritySettings = config.GetSection("SecuritySettings").Get<SecuritySettings>();
+    }
+
+    public Task<CreateBackendResponse> CreateCustomerBackend(CreateBackendRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<DeleteBackendResponse> DeleteCustomerBackend(DeleteBackendRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<DisableBackendResponse> DisableCustomerBackend(DisableBackendRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
