2f7ddfba8bce62534b7d9b8e5df317990624da07
Jakob Nyström <jakob.nystrom@addiva.se>
2023-04-08 23:04:56 +0200

    fix

--- src/Infrastructure/CDCI/CDCIService.cs
+++ src/Infrastructure/CDCI/CDCIService.cs
@@ -1,10 +1,7 @@
 ﻿using FSH.Core.Common;
-using FSH.Core.Dto.BankId;
 using FSH.Core.Dto.CDCI;
 using FSH.WebApi.Infrastructure.Common.Settings;
-using FSH.WebApi.Infrastructure.Helpers;
 using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
 using Serilog;
 
 namespace FSH.Infrastructure.Auth;
@@ -13,7 +10,7 @@ public class CDCIService : ICDCIService {
 
     private readonly ILogger Logger;
 
-    public CDCIService(IConfiguration config, ILogger logger, IUserService userService) {
+    public CDCIService(IConfiguration config, ILogger logger) {
         Logger = logger;
         Runtime.SecuritySettings = config.GetSection("SecuritySettings").Get<SecuritySettings>();
     }
